26d0df7caf7689a064ab9fc613fce83a3e9489ad
sksaifee <sksaifee0204@gmail.com>
2026-05-13 02:56:50 +0530

    Update App.jsx

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -4,9 +4,8 @@ function app(){
   return (
     <div>
       <img src={logo} alt="logo" />
-      <p>Base line.</p>
+      <p>New line added.</p>
       <h1 className="heading">I am Sk Saifee, I am a React developer.</h1>
-      
     </div>
   )
 }
