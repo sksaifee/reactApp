e77e3339ebdc15120418a0203ee3dbe38bd84127
sksaifee <sksaifee0204@gmail.com>
2026-05-13 02:31:42 +0530

    Update App.jsx

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 function app(){
   return (
-    <h1 className="heading">I am Sk Saifee, I am a Frontend react developer.</h1>
+    <h1 className="heading">I am Sk Saifee, I am a Frontend React dev.</h1>
   )
 }
 export default app;
