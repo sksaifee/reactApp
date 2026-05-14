56e082dd5ff3a97a59d8b184413a6d2d32af3442
sksaifee <sksaifee0204@gmail.com>
2026-05-15 00:44:01 +0530

    g

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -5,6 +5,7 @@ function app(){
     <div>
       <img src={logo} alt="logo"/>
       <h1 className="heading">Tamanna's Hut Of Purity.</h1>
+      <h2>Welcome to Tamanna's Hut Of Purity.</h2>
       <p>This is a website for Tamanna's Hut Of Purity.</p>
     </div>
   )
